d019d974e6b8442c119fbd2cc6a5ac7f2e07004a
DimaNinyuk <34214183+DimaNinyuk@users.noreply.github.com>
2017-12-03 19:59:22 +0200

    ths
    ssssh

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+namespace ближе_к_10
+{
+    class Program
+    {
+        int x, y;
+        public static int Abs(int a)
+        {
+            
+            if (a < 0) { a = -a; return a; }
+            else
+            {
+                a = a;
+ }      
+    return a;       
+        }
+        public void Vvod()
+        {
+            Console.WriteLine("введите первое число ");
+             x = int.Parse(Console.ReadLine());
+            Console.WriteLine("введите второе число  ");
+             y = int.Parse(Console.ReadLine());
+        }
+            public void Metr()
+            {
+            if (Abs(10 - x) > Abs(10 - y))  
+            {
+                Console.WriteLine("ближе к десети = " + y);
+            }
+            if (Abs(10 - x) < Abs(10 - y))
+                Console.WriteLine("ближе к десети = " + x);
+         }
+        static void Main(string[] args)
+        {
+            Program exz = new Program();
+            exz.Vvod();
+            exz.Metr();
+            Console.ReadKey();}}}
